c1159bfb879be320e986de1a476cf3404b8faed4
DeveloperDolon <dolonr718@gmail.com>
2025-01-30 23:23:43 +0600

    route error fix and project run again

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/sanctum/csrf-cookie', '\Laravel\Sanctum\Http\Controllers\CsrfCookieController@show');
+
 Route::get('/', function () {
     return view('welcome');
 });
