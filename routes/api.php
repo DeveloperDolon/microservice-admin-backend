f146e9b1884ac7761ddc85ca4f12be685ca0cdae
DeveloperDolon <dolonr718@gmail.com>
2025-02-01 22:08:40 +0600

    product api added

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
