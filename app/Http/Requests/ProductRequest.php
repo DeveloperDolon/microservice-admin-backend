dbd4816dd127df3951bc2fa3117c4b0a4d18fa7a
Dolon Roy <dolonr718@gmail.com>
2025-03-13 12:29:01 +0600

    product request added

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -6,23 +6,32 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProductRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+    protected $condition;
+
+    public function __construct()
+    {
+        $this->condition = $this->getMethod() === 'POST' ? 'required' : 'nullable';
+    }
+
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            
+            'name' => $this->condition . '|string|max:255|min:5',
+            'images' => $this->condition . '|string',
+            'discount' => $this->condition . '|numeric',
+            'price' => $this->condition . '|numeric',
+            'description' => $this->condition . 'string',
+            'discount_type' => $this->condition . '|enum:percentage,amount',
+            'likes' => $this->condition . '|integer',
+            'ingredients' => 'nullable|string',
+            'shipping_cost' => $this->condition . '|integer',
+            'benefit' => 'nullable|string',
+            'seller_id' => $this->condition . '|string',
         ];
     }
 }
