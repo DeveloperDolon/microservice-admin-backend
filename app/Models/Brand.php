34f5257195bdf10ac83793814b66422781ef3e6c
Dolon Roy <dolonr718@gmail.com>
2025-03-13 13:30:08 +0600

    brand model added

---
+++ app/Models/Brand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+        'banner',
+        'logo',
+        'title',
+        'description',
+        'location'
+    ];
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+}
