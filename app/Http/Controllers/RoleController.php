2e7ab77a81cd7bbc05848555385dbd44c79fc78a
DeveloperDolon <dolonr718@gmail.com>
2025-03-14 23:13:58 +0600

    role controller added

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RoleRequest;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
-class RoleController extends Controller
+class RoleController extends BaseController
 {
-    //
+    public function storeRole(RoleRequest $request)
+    {
+        $roleInfo = $request->validated();
+
+        $data = Role::create($roleInfo);
+
+        return $this->sendSuccessResponse($data);
+    }
 }
