c1e41ca5d121fbb0a38cf09ec579b2983d1674c3
DeveloperDolon <dolonr718@gmail.com>
2025-03-14 20:21:47 +0600

    base controller added

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function sendSuccessResponse($data, $message = 'Success!', $status = Response::HTTP_OK)
+    {
+        return [
+            'success' => true,
+            '$status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ];
+    }
+
+    public function sendErrorResponse($data = null, $message = 'Bad Request!', $status = Response::HTTP_BAD_REQUEST)
+    {
+        return [
+            'success' => false,
+            '$status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ];
+    }
+}
