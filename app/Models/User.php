62b878b727ca72ea345df1d19593c5017e7f4eb0
DeveloperDolon <dolonr718@gmail.com>
2025-03-14 12:22:46 +0600

    some changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,21 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $keyType = 'string';
-    public $incrementing = false;
-
+    
     protected $fillable = [
         'name',
         'email',
@@ -30,21 +23,12 @@ class User extends Authenticatable
         'profile_picture',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+   
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
