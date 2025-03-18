b30fca2435b2daeab522d24b97aa6e76bdb129c2
DeveloperDolon <dolonr718@gmail.com>
2025-03-18 11:01:41 +0600

    app configurred

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,6 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        commands: __DIR__ . '/../routes/console.php',
-        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
