a93b5979db2aa88bc35ddc09ab06b16a2269316a
DeveloperDolon <dolonr718@gmail.com>
2025-03-21 20:19:57 +0600

    migration changed for migrate error

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -8,6 +8,14 @@ return new class extends Migration
 {
     public function up(): void
     {
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->text('permissions');
+            $table->timestamps();
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
@@ -23,6 +31,7 @@ return new class extends Migration
    
     public function down(): void
     {
+        Schema::dropIfExists('roles');
         Schema::dropIfExists('users');
     }
 };
