c38a1e621bb4ac1481758bb886b20ec97fcda814
DeveloperDolon <dolonr718@gmail.com>
2025-03-21 21:59:20 +0600

    order list controller method added

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Order;
 
-class OrderController extends Controller
+class OrderController extends BaseController
 {
-    //
+    public function list()
+    {
+        $seller_id = request()->user()->id;
+
+        $orders = Order::whereHas('items.product', function ($query) use ($seller_id) {
+            $query->where('seller_id', $seller_id);
+        })->with('items')->paginate(10);
+
+        return $this->sendSuccessResponse($orders, 'Orders retrieved successfully!');
+    }
 }
