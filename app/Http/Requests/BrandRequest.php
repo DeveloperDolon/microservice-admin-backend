e9959ba40ff563e320c5a2a9e1a8ccaa2249f098
DeveloperDolon <dolonr718@gmail.com>
2025-03-29 21:22:54 +0600

    product request updated

--- app/Http/Requests/BrandRequest.php
+++ app/Http/Requests/BrandRequest.php
@@ -17,8 +17,8 @@ class BrandRequest extends FormRequest
         $condition = $this->getMethod() === 'POST' ? 'required' : 'nullable';
         return [
             'name' => $condition . '|string|max:255|min:5',
-            'banner' => 'nullable|string|max:255|min:5',
-            'logo' => $condition . '|string|max:255|min:5',
+            'banner' => 'nullable|file|mimes:jpeg,png,jpg,gif|max:2048',
+            'logo' => $condition . '|file|mimes:jpeg,png,jpg,gif|max:2048',
             'title' => $condition . '|string|max:255|min:5',
             'description' => 'nullable|string|max:255|min:5',
             'location' => 'nullable|string|max:255|min:5',
--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -17,11 +17,11 @@ class ProductRequest extends FormRequest
         $condition = $this->getMethod() === 'POST' ? 'required' : 'nullable';
         return [
             'name' => $condition . '|string|max:255|min:5',
-            'images' => $condition . '|string',
+            'images.*' => $condition . '|file|mimes:jpeg,png,jpg,gif|max:2048',
             'discount' => $condition . '|numeric',
             'price' => $condition . '|numeric',
             'description' => $condition . 'string',
-            'discount_type' => $condition . '|enum:percentage,amount',
+            'discount_type' => $condition . '|in:percentage,amount',
             'likes' => $condition . '|integer',
             'ingredients' => 'nullable|string',
             'shipping_cost' => $condition . '|integer',
