d65e64f684a8fe57488188f0289630b722f9c182
DeveloperDolon <dolonr718@gmail.com>
2025-03-31 23:02:22 +0600

    rabbit mq conversion configuration successfully added

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BrandRequest;
 use App\Jobs\BrandCreateJob;
-use App\Jobs\TestJob;
 use App\Models\Brand;
 
 class BrandController extends BaseController
