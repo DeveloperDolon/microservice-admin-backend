c25c54ddece38788ff4422e5d83fd2a90a4a9de2
DeveloperDolon <dolonr718@gmail.com>
2025-03-31 10:12:00 +0600

    brand create job created

---
+++ app/Jobs/BrandCreateJob.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class BrandCreateJob implements ShouldQueue
+{
+    use Queueable;
+
+    private $data;
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function handle(): void
+    {
+        
+    }
+}
